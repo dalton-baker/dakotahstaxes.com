afce9c6fdba2732e7f82104b26f33a110dc329c1
Dalton Baker <dalton.s.baker@gmail.com>
2021-03-19 23:25:02 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dakotah's Bookkeeping & Taxes</title>
+</head>
+<style>
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+    }
+
+    .bgimg {
+        background-image: url('/assets/background.jpeg');
+        height: 100%;
+        background-position: center;
+        background-size: cover;
+        position: relative;
+        color: black;
+        font-family: "Courier New", Courier, monospace;
+        font-size: 32px;
+        font-weight: bold;
+        text-shadow:
+            -1px -1px 0 #fff,
+            1px -1px 0 #fff,
+            -1px 1px 0 #fff,
+            1px 1px 0 #fff;
+    }
+
+    .topleft {
+        position: absolute;
+        top: 0;
+        left: 16px;
+    }
+
+    .topright {
+        position: absolute;
+        top: 0;
+        right: 16px;
+    }
+
+
+    .bottomright {
+        position: absolute;
+        bottom: 0;
+        right: 16px;
+    }
+
+    .bottomleft {
+        position: absolute;
+        bottom: 0;
+        left: 16px;
+    }
+
+    .middle {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+
+    hr {
+        margin: auto;
+        width: 40%;
+    }
+</style>
+
+<body>
+
+    <div class="bgimg">
+        <div class="topleft">
+            <p>Dakotah's Bookkeeping & Taxes</p>
+        </div>
+        <div class="topright">
+            <p>(724) 638-7495</p>
+        </div>
+        <div class="middle">
+            <h1>COMING SOON</h1>
+        </div>
+        <div class="bottomleft">
+            <p>Over 20 years experience</p>
+            <p>taxwizard8@gmail.com</p>
+        </div>
+
+        <div class="bottomright">
+            <img src="/assets/cert-agent.jpg" height="100">
+        </div>
+    </div>
+
+</body>
+
+</html>
