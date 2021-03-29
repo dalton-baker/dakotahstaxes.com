980bc28b45932127f7affb9bf7ceffd64d6f9b8e
Dalton Baker <dalton.s.baker@gmail.com>
2021-03-29 00:17:15 -0600

    Added tabbed content and a general layout

--- index.php
+++ index.php
@@ -1,94 +1,72 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>Dakotah's Bookkeeping & Taxes</title>
-</head>
 <style>
-    body,
-    html {
-        height: 100%;
-        margin: 0;
-    }
-
-    .bgimg {
-        background-image: url('/assets/background.jpeg');
-        height: 100%;
-        background-position: center;
-        background-size: cover;
-        position: relative;
-        color: black;
-        font-family: "Courier New", Courier, monospace;
-        font-size: 32px;
-        font-weight: bold;
-        text-shadow:
-            -1px -1px 0 #fff,
-            1px -1px 0 #fff,
-            -1px 1px 0 #fff,
-            1px 1px 0 #fff;
-    }
-
-    .topleft {
-        position: absolute;
-        top: 0;
-        left: 16px;
-    }
-
-    .topright {
-        position: absolute;
-        top: 0;
-        right: 16px;
-    }
-
-
-    .bottomright {
-        position: absolute;
-        bottom: 0;
-        right: 16px;
-    }
-
-    .bottomleft {
-        position: absolute;
-        bottom: 0;
-        left: 16px;
-    }
-
-    .middle {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-    }
-
-    hr {
-        margin: auto;
-        width: 40%;
-    }
+    <?php include 'index.css'; ?>
 </style>
 
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dakotah's Bookkeeping & Taxes</title>
+</head>
+
 <body>
+    <input type="radio" name="menu_toggle" id="home-tog" checked="checked">
+    <input type="radio" name="menu_toggle" id="sched-tog">
+    <input type="radio" name="menu_toggle" id="info-tog">
+    <input type="radio" name="menu_toggle" id="tool-tog">
+    <input type="radio" name="menu_toggle" id="news-tog">
 
-    <div class="bgimg">
-        <div class="topleft">
-            <p>Dakotah's Bookkeeping & Taxes</p>
-        </div>
-        <div class="topright">
-            <p>(724) 638-7495</p>
-        </div>
-        <div class="middle">
-            <h1>COMING SOON</h1>
-        </div>
-        <div class="bottomleft">
-            <p>Over 20 years experience</p>
-            <p>taxwizard8@gmail.com</p>
+    <div id="title-bar">
+        <div id="title">
+            <div>Dakotah's Bookkeeping & Taxes</div>
         </div>
 
-        <div class="bottomright">
-            <img src="/assets/cert-agent.jpg" height="100">
+        <div id="menu-bar">
+            <label for="home-tog" id="home-label" tabindex="1"><i class="material-icons">home</i> Home</label>
+            <label for="sched-tog" id="sched-label" tabindex="2"><i class="material-icons">schedule</i> Scheduling</label>
+            <label for="info-tog" id="info-label" tabindex="3"><i class="material-icons">info</i> Tax Info</label>
+            <label for="tool-tog" id="tool-label" tabindex="4"><i class="material-icons">construction</i> Tax Tools</label>
+            <label for="news-tog" id="news-label" tabindex="5"><i class="material-icons">web</i> Tax News</label>
         </div>
     </div>
 
+    <div id="tab-content">
+        <div id="home-content">
+            <p>Welcome to Dakotah's Bookkeeping & Taxes!</p>
+            <p>This site is still under construction.</p>
+            <p>You can still contact us with the info at the bottom of the page.</p>
+        </div>
+
+        <div id="sched-content">
+            <p>Scheduling Content - Under Construction</p>
+        </div>
+
+        <div id="info-content">
+            <p>Tax Info - Under Construction</p>
+        </div>
+
+        <div id="tool-content">
+            <p>Tax Tools - Under Construction</p>
+        </div>
+
+        <div id="news-content">
+            <p>Tax News - Under Construction</p>
+        </div>
+    </div>
+
+    <footer id="footer">
+        <div id="footer-contact">
+            <div>CONTACT US</div>
+            <!-- <div>Address</div> -->
+            <div>PHONE: (724) 638-7495</div>
+            <div>FAX: (724) 835-8524</div>
+            <div>EMAIL: taxwizard8@gmail.com</div>
+        </div>
+
+        <img src="/assets/cert-agent.jpg" height="100">
+    </footer>
 </body>
 
 </html>
